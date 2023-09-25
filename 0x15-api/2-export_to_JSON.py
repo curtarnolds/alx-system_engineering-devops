b5c352ab4472392f1f5dab5398003af5c4703306
curtarnolds <afayedormawuli@gmail.com>
2023-09-25 22:26:17 +0000

    2. Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Returns information about an Employee's TODO progress given their id"""
+import json
+from sys import argv
+from urllib import request
+
+
+if __name__ == '__main__':
+    UserId = argv[1]
+    response = json.loads(request.urlopen(
+        f'https://jsonplaceholder.typicode.com/todos?userId={UserId}').read()
+        .decode('utf-8'))
+
+    username = json.loads(request.urlopen(
+        f'https://jsonplaceholder.typicode.com/users/{UserId}').read()
+        .decode('utf-8')).get('username')
+    with open(f'{UserId}.json', 'wt') as json_file:
+        _dict = {f'{UserId}': []}
+        for res in response:
+            _dict[f'{UserId}'].append(
+                {
+                    'task': f"{res.get('title')}",
+                    'completed': f"{res.get('completed')}",
+                    'username': f'{username}'
+                }
+            )
+        json.dump(_dict, json_file)
