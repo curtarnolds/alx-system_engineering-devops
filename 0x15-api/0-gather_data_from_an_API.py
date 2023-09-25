d76155d6041267f6a88a23069ee42d0f31429e92
curtarnolds <afayedormawuli@gmail.com>
2023-09-25 20:59:41 +0000

    0. Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Returns information about an Employee's TODO progress given their id"""
+from sys import argv
+import json
+from urllib import request
+
+
+UserId = argv[1]
+response = json.loads(request.urlopen(
+    f'https://jsonplaceholder.typicode.com/todos?userId={UserId}').read()
+    .decode('utf-8'))
+
+emp_name = json.loads(request.urlopen(
+    f'https://jsonplaceholder.typicode.com/users/{UserId}').read()
+    .decode('utf-8')).get('name')
+done_tasks = len([comp for comp in response if comp.get('completed') is True])
+total_tasks = len(response)
+
+
+print(
+    f"Employee {emp_name} is done with tasks({done_tasks}/{total_tasks}):"
+)
+for res in response:
+    print('\t', res.get('title'))
